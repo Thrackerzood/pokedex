8c1dc1788b929e27d1974513c5dd2f191e166f3b
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-09-10 05:47:17 +0400

    Add files via upload

--- components/pokedexContent.tsx
+++ components/pokedexContent.tsx
@@ -10,7 +10,9 @@ import { Pokemon, TypePokemonArr } from '../interface/typePokemonInterface';
 
 
 const PokedexContent:NextPage = () => {
-
+    const initialPokemonValue:TypePokemonArr = {
+        "id": 1, "generation": { "name": "", "url": "" }, "name": "", "names": [ { "language": { "name": "", "url": "" }, "name": "" }, { "language": { "name": "", "url": "" }, "name": "" } ], "damage_relations": { "double_damage_from": [ { "name": "", "url": "" } ], "double_damage_to": [], "half_damage_from": [], "half_damage_to": [ { "name": "", "url": "" }, { "name": "", "url": "" } ], "no_damage_from": [ { "name": "", "url": "" } ], "no_damage_to": [ { "name": "", "url": "" } ] }, "game_indices": [ { "game_index": 0, "generation": { "name": "", "url": "" } }, { "game_index": 0, "generation": { "name": "", "url": "" } } ], "move_damage_class": { "name": "", "url": "" }, "moves": [ { "name": "", "url": "" }, { "name": "", "url": "" }, { "name": "", "url": "" } ], "past_damage_relations": [], "pokemon": [ { "pokemon": { "name": "", "url": "" }, "slot": 1 }, { "pokemon": { "name": "", "url": "" }, "slot": 1 }, { "pokemon": { "name": "", "url": "" }, "slot": 1 } ] 
+       }
     const pokemonUrl:TypePokemonArr = useContext(ContextPokemon)
     const pokemon:string = useContext(Contexts)
     const [data, setData]:[{data: IData},Dispatch<SetStateAction<{data: IData}>>] = useState({
@@ -19,20 +21,11 @@ const PokedexContent:NextPage = () => {
     const [allPokemon, setPokemon]:[{data: IPokemon[]},Dispatch<SetStateAction<{data: IPokemon[]}>>] = useState({data:
         [{ "abilities": [ { "ability": { "name": "", "url": "" }, "is_hidden": false, "slot": 1 }, { "ability": { "name": "", "url": "" }, "is_hidden": true, "slot": 3 } ], "description": "", "base_experience": 64, "forms": [ { "name": "bulbasaur", "url": "" } ], "game_indices": [ { "game_index": 153, "version": { "name": "red", "url": "" } }, { "game_index": 153, "version": { "name": "blue", "url": "" } } ], "height": 7, "held_items": [], "id": 1, "is_default": true, "location_area_encounters": "", "moves": [ { "move": { "name": "", "url": "" }, "version_group_details": [ { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } } ] }, { "move": { "name": "", "url": "" }, "version_group_details": [ { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "/" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "machine", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "x-y", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } }, { "level_learned_at": 0, "move_learn_method": { "name": "", "url": "" }, "version_group": { "name": "", "url": "" } } ] } ], "name": "", "order": 1, "past_types": [], "species": { "name": "", "url": "" }, "sprites": { "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null, "other": { "dream_world": { "front_default": "", "front_female": null }, "official-artwork": { "front_default": "" } }, "versions": { "generation-i": { "red-blue": { "back_default": "", "back_gray": "", "front_default": "", "front_gray": "" }, "yellow": { "back_default": "", "back_gray": "", "front_default": "", "front_gray": "" } }, "generation-ii": { "crystal": { "back_default": "", "back_shiny": "", "front_default": "", "front_shiny": "" }, "gold": { "back_default": "", "back_shiny": "", "front_default": "", "front_shiny": "" }, "silver": { "back_default": "", "back_shiny": "", "front_default": "", "front_shiny": "" } }, "generation-iii": { "emerald": { "front_default": "", "front_shiny": "" }, "firered-leafgreen": { "back_default": "", "back_shiny": "", "front_default": "", "front_shiny": "" }, "ruby-sapphire": { "back_default": "", "back_shiny": "", "front_default": "", "front_shiny": "" } }, "generation-iv": { "diamond-pearl": { "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null }, "heartgold-soulsilver": { "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null }, "platinum": { "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null } }, "generation-v": { "black-white": { "animated": { "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null }, "back_default": "", "back_female": null, "back_shiny": "", "back_shiny_female": null, "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null } }, "generation-vi": { "omegaruby-alphasapphire": { "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null }, "x-y": { "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null } }, "generation-vii": { "icons": { "front_default": "", "front_female": null }, "ultra-sun-ultra-moon": { "front_default": "", "front_female": null, "front_shiny": "", "front_shiny_female": null } }, "generation-viii": { "icons": { "front_default": "", "front_female": null } } } }, "stats": [ { "base_stat": 45, "effort": 0, "stat": { "name": "", "url": "" } }, { "base_stat": 49, "effort": 0, "stat": { "name": "", "url": "" } } ], "types": [ { "slot": 1, "type": { "name": "", "url": "" } }, { "slot": 2, "type": { "name": "", "url": "" } } ], "weight": 69 }]
         })
-    const [load, setStateLoad]:[boolean | undefined,Dispatch<SetStateAction<boolean | undefined>>] = useState()
-    const [loadDataType, setLoadData]:[boolean | undefined | any,Dispatch<SetStateAction<boolean | undefined  | any>>]= useState(true)
+    const [load, setStateLoad]:[boolean,Dispatch<SetStateAction<boolean | undefined | any>>] = useState(false)
+    const [loadDataType, setLoadData]:[boolean,Dispatch<SetStateAction<boolean | undefined | any>>]= useState(true)
 
     const loadData = async() =>{
         setStateLoad(false)
-
-        const allPokemonPromise:IPokemon[] = await Promise.all(
-            await  pokemonUrl.pokemon.map(
-                async (pokemon:Pokemon) => {
-                    return await allGetPokemon(pokemon.pokemon.url)
-        }))
-
-        setPokemon({data: allPokemonPromise})
-        setStateLoad(true)
         if(pokemon){
             await setPokemon({data:[await ajax( `https://pokeapi.co/api/v2/pokemon/${pokemon}` , 'GET')]})
             setStateLoad(true)
@@ -48,6 +41,15 @@ const PokedexContent:NextPage = () => {
             setPokemon({data: allPokemonPromise})
             setStateLoad(true)
             setLoadData(false)
+        }
+        if(pokemonUrl){
+            const allPokemonPromise:IPokemon[] = await Promise.all(
+                await  pokemonUrl.pokemon.map(
+                    async (pokemon:Pokemon) => {
+                        return await allGetPokemon(pokemon.pokemon.url)
+            }))
+            setPokemon({data: allPokemonPromise})
+            setStateLoad(true)
         }
     }
 
@@ -88,7 +90,6 @@ const PokedexContent:NextPage = () => {
 
     useEffect(() => {
         loadData()
-        setStateLoad(false)
     }, [pokemon , pokemonUrl])
 
     return(<>
